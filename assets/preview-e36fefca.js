b00b74ebe2dcfca513c929758fe90a5fdc0d8c81
joehecn <joehecn@users.noreply.github.com>
2023-09-27 05:47:15 +0000

    deploy: a19a092d8b40aad9c77b596dbfa30a99ffec2958

---
+++ assets/preview-e36fefca.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e045ad0d.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-627a5b19.js"),["./DocsRenderer-3PUGWF3O-627a5b19.js","./iframe-e045ad0d.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-38063e1e.js","./index-ffc7e5ff.js","./index-fb8a4827.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-e36fefca.js.map
